58380ed20479de4cb58fa10b0cd62d7e4f5f6603
leventecsipak <levente@alum.lehigh.edu>
2020-12-19 11:26:53 +0100

    some adjustments

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm5')
+    print_hi('PyCharm6')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
