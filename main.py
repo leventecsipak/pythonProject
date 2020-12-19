9588b11c05b49930e03ea16e2db4bcc0222c0366
leventecsipak <levente@alum.lehigh.edu>
2020-12-19 11:47:41 +0100

    minor fixes

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm8')
+    print_hi('PyCharm9')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
